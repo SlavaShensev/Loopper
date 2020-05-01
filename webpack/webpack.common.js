9ea22b90095dc268b75125e09301fbabf5882597
dl <weblevin@gmail.com>
2020-05-01 16:04:56 +0300

    Init project

---
+++ webpack/webpack.common.js
@@ -0,0 +1,91 @@
+const Path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: Path.resolve(__dirname, '../src/scripts/index.js')
+    },
+    output: {
+        path: Path.join(__dirname, '../build'),
+        filename: 'js/[name].js'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: false
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {from: Path.resolve(__dirname, '../public'), to: 'public'}
+        ]),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: Path.resolve(__dirname, '../src/index.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            template: Path.resolve(__dirname, '../src/about.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'services.html',
+            template: Path.resolve(__dirname, '../src/services.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'team.html',
+            template: Path.resolve(__dirname, '../src/team.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'gallery.html',
+            template: Path.resolve(__dirname, '../src/gallery.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'contact.html',
+            template: Path.resolve(__dirname, '../src/contact.html'),
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~': Path.resolve(__dirname, '../src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto'
+            },
+            {
+                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    }
+                }
+            },
+        ]
+    }
+};
